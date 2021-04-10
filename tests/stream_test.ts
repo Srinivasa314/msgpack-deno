d156693aa882a42bc32453e40378a7698f67cae0
Srinivasa Mahesh <62195746+Srinivasa314@users.noreply.github.com>
2021-04-10 17:15:25 +0530

    Fix grammar

--- tests/stream_test.ts
+++ tests/stream_test.ts
@@ -2,14 +2,14 @@ import { assertEquals } from "https://deno.land/std@0.68.0/testing/asserts.ts";
 
 import { decodeStream, encode } from "../mod.ts";
 
-Deno.test("Test decodeStream properly handle complete data", async () => {
+Deno.test("Test whether decodeStream properly handles complete data", async () => {
   const reader = new Deno.Buffer(encode("0123456789"));
   const stream = Deno.iter(reader, { bufSize: 10 });
   const result = await consumeAll(decodeStream(stream));
   assertEquals(result, ["0123456789"]);
 });
 
-Deno.test("Test decodeStream properly handle incomplete data", async () => {
+Deno.test("Test whether decodeStream properly handles incomplete data", async () => {
   const reader = new Deno.Buffer(encode("0123456789"));
   const stream = Deno.iter(reader, { bufSize: 8 });
   const result = await consumeAll(decodeStream(stream));
